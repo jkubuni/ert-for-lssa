47bb858f60a000706d8762ec899df506330c418d
jkubuni <jan.kubeler@student.hpi.uni-potsdam.de>
2025-12-17 15:47:20 +0100

    Add 5500U results

--- Roofline_Visualizer-1.1.0/rooflineVisualizer.sh
+++ Roofline_Visualizer-1.1.0/rooflineVisualizer.sh
@@ -1,1 +1,1 @@
-java -jar ./RooflineChart.jar 
+java --module-path /home/jonas/Documents/LSSA/ert-for-lssa/javafx-sdk/javafx-sdk-25.0.1/lib --add-modules javafx.controls,javafx.fxml,javafx.web,javafx.swing -jar ./RooflineChart.jar 
